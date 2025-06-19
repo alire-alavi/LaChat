41a758fa32a251655dab055ad8d61f7edcf9b4e8
alire-alavi <alirezaalavirazavi@yahoo.com>
2025-06-19 12:01:47 +0330

    update: switch to db url

--- group-chat-backend/src/app.module.ts
+++ group-chat-backend/src/app.module.ts
@@ -17,12 +17,7 @@ import { AppConfig } from './config/app-config.schema';
             inject: [ConfigService],
             useFactory: (configService: ConfigService<AppConfig>) => ({
                 type: 'postgres',
-                // url: configService.get<string>('DATABASE_URL'),
-                database: configService.get<string>('DATABASE_NAME'),
-                port: configService.get<number>('DATABASE_PORT'),
-                host: configService.get<string>('DATABASE_HOST'),
-                username: configService.get<string>('DATABASE_USER'),
-                password: configService.get<string>('DATABASE_PASSWORD'),
+                url: configService.get<string>('DATABASE_URL'),
                 autoLoadEntities: true,
                 synchronize: configService.get<boolean>('DATABASE_SYNC', true),
                 ssl: false,
